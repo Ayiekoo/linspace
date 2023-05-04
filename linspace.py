c75a5418af40ce700af88b9248e9aa9f7b2c06f0
Ayiekoo <131998667+Ayiekoo@users.noreply.github.com>
2023-05-05 02:51:05 +0300

    Add files via upload

---
+++ linspace.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create an array of 50 evenly spaced values between -5 and 5
+x = np.linspace(-5, 5, 50)
+
+# Compute the y values for each x value
+y = x**2
+
+# Plot the x and y values
+plt.plot(x, y)
+
+# Show the plot
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
